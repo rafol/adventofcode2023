0bfbbd5cc2ac8fbfa5cbd8d3c080e3031174cb64
Rafael S. Medeiros <rafael.medeiros@poatek.com>
2023-12-05 21:24:30 -0300

    :tada: First!

---
+++ day_01/main.py
@@ -0,0 +1,21 @@
+import pathlib as pl
+import numpy as np
+import regex as re
+
+
+# ### main args
+infile = pl.Path()
+
+# ### Secondary args (change wiselly...)
+
+
+# ### load inputs
+
+
+# ### process
+
+
+# ### print out
+
+
+print(f"DONE!")
